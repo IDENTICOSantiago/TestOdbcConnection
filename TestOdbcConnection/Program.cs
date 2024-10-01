3d53629a49b460dcc25b047771a1cc81876d0d62
davidherrera <dsherrera918@identico.com.co>
2024-10-01 10:45:15 -0500

    Agregar archivos de proyecto.

---
+++ TestOdbcConnection/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Data.Odbc;
+
+class Program
+{
+    static void Main()
+    {
+        string connectionString = "DSN=RFID_DB;UID=sa;PWD=Id.2009.Sistemas*;DATABASE=RFID;";
+
+
+        using (OdbcConnection connection = new OdbcConnection(connectionString))
+        {
+            try
+            {
+                connection.Open();
+                Console.WriteLine("Conexión exitosa");
+
+                string query = "SELECT TOP 50 * FROM dbo.Activos"; 
+
+                using (OdbcCommand command = new OdbcCommand(query, connection))
+                {
+                    using (OdbcDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Console.WriteLine($"ACT_ID: {reader["ACT_ID"]}, NOMBRE_ACTIVO: {reader["NOMBRE_ACTIVO"]}");
+                        }
+                    }
+                }
+            }
+            catch (OdbcException ex)
+            {
+                Console.WriteLine("Error de conexión: " + ex.Message);
+            }
+        }
+    }
+}
